035b7951f962cdce445e44c694878fcd92f613a7
imane <benbenimane@hotmail.com>
2020-06-15 14:32:58 +0200

    Updates

---
+++ precache-manifest.a385aa0ebcad5496aaab4aa0b04d98ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326c27065cdb02e353c7a38851ac6f9d",
+    "url": "/portfolio-workshop-react/index.html"
+  },
+  {
+    "revision": "3d1afd9a8e742683c81c",
+    "url": "/portfolio-workshop-react/static/css/main.cc0b3c8d.chunk.css"
+  },
+  {
+    "revision": "b815135644d5b4af1d39",
+    "url": "/portfolio-workshop-react/static/js/2.67049f6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-workshop-react/static/js/2.67049f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1afd9a8e742683c81c",
+    "url": "/portfolio-workshop-react/static/js/main.1b1f5f38.chunk.js"
+  },
+  {
+    "revision": "ed35d4f4c6a0ab6cf222",
+    "url": "/portfolio-workshop-react/static/js/runtime-main.c735fc40.js"
+  }
+]);
